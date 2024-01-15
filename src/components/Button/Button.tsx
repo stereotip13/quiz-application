c319279c00b7d320fd350f02d59223cfb23aa218
jsenjoyer <jsenjoyer@icloud.com>
2024-01-15 14:45:00 +0300

    component's structure init

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,15 @@
+import {Button as MUIButton} from "@mui/material";
+
+//read about MUI components https://mui.com/material-ui/react-button/
+
+
+interface ButtonProps {
+    disabled: boolean
+}
+
+export const Button = ({disabled}: ButtonProps) => {
+    return <MUIButton disabled={disabled}></MUIButton>
+}
+
+//preffer named export , not default one
+//each component has to have index file
