86843e1a446082f6b8f2e82124766cd37d5f36d7
stereotip13 <stereotip13@gmail.com>
2024-01-17 17:48:33 +0500

    first commit

---
+++ src/components/pages/AuthorizationPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from '../Button/index.ts';
+const authorizationPage = () => {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+};
+
+export default authorizationPage;
