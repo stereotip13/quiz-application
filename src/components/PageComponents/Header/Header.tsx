cd99e9530910b6570e43dbebb90989fbdffb68b6
stereotip13 <stereotip13@gmail.com>
2024-04-10 17:47:06 +0500

    change navbar, fix bug

--- src/components/PageComponents/Header/Header.tsx
+++ src/components/PageComponents/Header/Header.tsx
@@ -1,31 +1,28 @@
 import { NavLink } from 'react-router-dom';
 import '../../../../src/index.css';
-import { Box } from '@mui/material';
 
 
 export const Header = () => {
   const setActive = ({ isActive }: { isActive: boolean }) =>
     isActive ? 'active-link' : '';
   return (
-    <div className="header-link">
-      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Box sx={{ display: 'flex', justifyContent: 'flex-start', p: 2, m: 2 }}>
-          <NavLink className={setActive} to="/">
+    <nav className="header-link">
+       <div className="logo" >
+          <NavLink className={setActive} to="/" style={{color: 'white', marginLeft: "10px"}}>
             Главная
           </NavLink>
-        </Box>
-        <Box sx={{ display: 'flex', justifyContent: 'flex-end', p: 2, m: 2 ,'& a': {marginLeft:'15px'}}}>
-          <NavLink className={setActive} to="/quiz">
-            Начать тестирование
-          </NavLink>
-          <NavLink className={setActive} to="/result">
-            Результаты
-          </NavLink>
-          <NavLink className={setActive} to="/admin">
-            Панель управления
-          </NavLink>
-        </Box>
-      </Box>
-    </div>
+       </div>
+       <div className="nav-links">
+            <NavLink className={setActive} to="/quiz" style={{color: 'white'}}>
+              Начать тестирование
+            </NavLink>
+            <NavLink className={setActive} to="/result" style={{color: 'white', marginLeft: "20px"}}>
+              Результаты
+            </NavLink>
+            <NavLink className={setActive} to="/admin" style={{color: 'white', marginLeft: "20px", marginRight: "20px" }}>
+              Панель управления
+            </NavLink>
+        </div>
+    </nav>
   );
 };
