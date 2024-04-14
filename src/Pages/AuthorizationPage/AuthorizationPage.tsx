42d27a48d3c8e6b692d2074418bb266aced66742
stereotip13 <stereotip13@gmail.com>
2024-04-14 22:38:06 +0500

    add certificate component

--- src/Pages/AuthorizationPage/AuthorizationPage.tsx
+++ src/Pages/AuthorizationPage/AuthorizationPage.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '../../components/Button';
+import { createAttachedSignature } from 'crypto-pro';
 //import Button from '@mui/material/Button';
 export const AuthorizationPage = () => {
   const navigate = useNavigate();
@@ -8,8 +9,40 @@ export const AuthorizationPage = () => {
     e.preventDefault();
     navigate('/quiz', { replace: true });
   };
+  const [message, setMessage] = useState('');
+  const [certificate, setCertificate] = useState(null);
+  const [hashStatus, setHashStatus] = useState('Не вычислен');
+  const [hashError, setHashError] = useState(null);
+  const [signature, setSignature] = useState('');
+  const [signatureStatus, setSignatureStatus] = useState('Не создана');
+  const [signatureError, setSignatureError] = useState(null);
+  async function createSignature(event) {
+    event.preventDefault();
+
+    setSignature('');
+    setSignatureError(null);
+
+    setHash('');
+    setHashError(null);
+    setHashStatus('Вычисляется...');
+
+    setHashStatus('Не вычислен');
+    setSignatureStatus('Создается...');
+
+    try {
+      setSignature(
+        await createAttachedSignature(certificate.thumbprint, message),
+      );
+      console.log(signature);
+      navigate('/quiz', { replace: true });
+    } catch (error) {
+      setSignatureError(error.message);
+    }
+
+    setSignatureStatus('Не создана');
+  }
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={createAttachedSignature}>
       <h1>АВТОРИЗАЦИЯ ДЛЯ НАЧАЛА ТЕСТА</h1>
       <Button disabled={false} type="submit" />
 
